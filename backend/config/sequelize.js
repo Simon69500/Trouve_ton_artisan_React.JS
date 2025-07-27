a50ca3c89d6a7ded3fd3c54a6904ec0bdd5ee976
Simon69500 <150716564+Simon69500@users.noreply.github.com>
2025-07-27 20:49:46 +0200

    config server.js pour hebergement

--- backend/config/sequelize.js
+++ backend/config/sequelize.js
@@ -1,15 +1,16 @@
 const {Sequelize} = require('sequelize');
+require ('dotenv').config();
 
-// Connexion à la base de données PostgreSQL
+
+// Connexion à la base de données MySQL
 const sequelize = new Sequelize (
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-        host: process.env.DB_HOST,
-        port: Number(process.env.DB_PORT) || 5432,
-        dialect: 'postgres',
-        logging: false,
+        host: process.env.DB_HOST || 'localhost',
+        port: Number(process.env.DB_PORT) || 3306 ,
+        dialect: 'mysql',
     }
 );
 
