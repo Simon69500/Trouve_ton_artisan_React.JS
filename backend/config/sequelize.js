71bf5c51b0751743e86cefe7fdaf522fb70691b8
Simon69500 <150716564+Simon69500@users.noreply.github.com>
2025-07-27 21:10:15 +0200

    config sequelize.js pour deploy

--- backend/config/sequelize.js
+++ backend/config/sequelize.js
@@ -4,12 +4,12 @@ require ('dotenv').config();
 
 // Connexion à la base de données MySQL
 const sequelize = new Sequelize (
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.MYSQL_DATABASE,
+    process.env.MYSQLUSER,
+    process.env.MYSQL_ROOT_PASSWORD,
     {
-        host: process.env.DB_HOST || 'localhost',
-        port: Number(process.env.DB_PORT) || 3306 ,
+        host: process.env.MYSQLHOST || 'localhost',
+        port: Number(process.env.MYSQLPORT) || 3306 ,
         dialect: 'mysql',
     }
 );
