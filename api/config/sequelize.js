eb3434934d48608ec20dbaf277e0fe0b581d879e
Simon69500 <150716564+Simon69500@users.noreply.github.com>
2025-07-27 15:21:54 +0200

    Reglages pour server.js pour hebergement

--- api/config/sequelize.js
+++ api/config/sequelize.js
@@ -1,6 +1,11 @@
 const {Sequelize} = require('sequelize');
 require ('dotenv').config();
 
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_PORT:', process.env.DB_PORT);
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_USER:', process.env.DB_USER);
+
 
 // Connexion à la base de données MySQL
 const sequelize = new Sequelize (
@@ -9,9 +14,15 @@ const sequelize = new Sequelize (
     process.env.DB_PASSWORD,
     {
         host: process.env.DB_HOST,
-        port: process.env.DB_PORT || 5432,
+        port: Number(process.env.DB_PORT) || 5432,
         dialect: 'postgres',
         logging: false,
+        dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
     }
 );
 
