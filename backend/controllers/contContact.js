4174f93109d2eff98c743cb1625bc6e3dca86a32
Simon69500 <150716564+Simon69500@users.noreply.github.com>
2025-07-28 19:16:08 +0200

    version final

--- backend/controllers/contContact.js
+++ backend/controllers/contContact.js
@@ -1,16 +1,29 @@
 const contactService = require('../services/servicesContact');
 
 exports.sendEmail = async (req, res) => {
-    try {
-        const {nom, email, objet, message} = req.body ;
+  try {
+    const { nom, email, objet, message } = req.body;
 
-        if(!nom || !email || !objet || !message) {
-            return res.status(400).json({message: 'Tous les champs sont requis !'});
-        }
-
-        await contactService.sendMail({ nom, email,objet, message})
-        res.status(200).json({message: 'Email envoyé avec succés !'})
-    } catch(error) {
-        res.status(500).json({message: 'Erreur serveur', error});
+    if (!nom || !email || !objet || !message) {
+      return res.status(400).json({
+        error: true,
+        message: 'Tous les champs sont requis !'
+      });
     }
+
+    await contactService.sendMail({ nom, email, objet, message });
+
+    res.status(200).json({
+      success: true,
+      message: 'Email envoyé avec succès !'
+    });
+
+  } catch (error) {
+    console.error("Erreur lors de l'envoi de l'email :", error);
+
+    res.status(500).json({
+      error: true,
+      message: 'Erreur serveur, veuillez réessayer plus tard.'
+    });
+  }
 };
