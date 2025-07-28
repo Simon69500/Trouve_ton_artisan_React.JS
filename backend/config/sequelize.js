1fe97d4a51e263a27c8c693b9888d276b8577baf
Simon69500 <150716564+Simon69500@users.noreply.github.com>
2025-07-28 15:45:04 +0200

    add .env.local

--- backend/config/sequelize.js
+++ backend/config/sequelize.js
@@ -1,28 +1,33 @@
-const {Sequelize} = require('sequelize');
-require ('dotenv').config();
+const { Sequelize } = require('sequelize');
+require('dotenv').config({ path: '.env.local' });
 
-
-// Connexion à la base de données MySQL
-const sequelize = new Sequelize (
-    process.env.MYSQL_DATABASE,
-    process.env.MYSQLUSER,
-    process.env.MYSQL_ROOT_PASSWORD,
-    {
-        host: process.env.MYSQLHOST || 'localhost',
-        port: Number(process.env.MYSQLPORT) || 3306 ,
-        dialect: 'mysql',
-    }
+// Configuration Sequelize
+const sequelize = new Sequelize(
+  process.env.MYSQL_DATABASE,
+  process.env.MYSQLUSER,
+  process.env.MYSQL_ROOT_PASSWORD,
+  {
+    host: process.env.MYSQLHOST || 'localhost',
+    port: Number(process.env.MYSQLPORT) || 3306,
+    dialect: 'mysql',
+  }
 );
 
 async function testConnection() {
-    try{
-        await sequelize.authenticate();
-        console.log('Connexion à la BDD réussite !');
-    } catch(error){
-        console.error('Impossible de se connecter à la BDD : ', error);
-    }
+  try {
+    await sequelize.authenticate();
+    console.log('Connexion à la BDD réussite !');
+  } catch (error) {
+    console.error('Impossible de se connecter à la BDD : ', error);
+  }
 }
 
 testConnection();
 
-module.exports = sequelize ;
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Serveur backend lancé sur le port ${PORT}`));
+
+module.exports = sequelize;
